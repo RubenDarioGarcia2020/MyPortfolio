98c86265902cb6bd05625423d5ca19774ac4ff47
ru_garcia2021 <rubendariogarcia@hotmail.com>
2021-01-29 11:06:00 -0300

    Modifica para muchos saludos

--- HolaWorld.py
+++ HolaWorld.py
@@ -2,5 +2,15 @@
 print("Ingrese nombre de la persona a quien saludar: \n")
 nom_persona=input()
 apel_persona=input("Ingrese apellido de la persona a quien saludar: \n")
-print("Hola, cómo estás ", nom_persona, apel_persona, "?")
+print("Hola, cómo estás", nom_persona, apel_persona, "?")
 
+print("\nDesea saludar a otra persona? : \n")
+otro_saludo=int(input("ingrese 1 en caso afirmativo - 0 para salir: --> "))
+
+while otro_saludo == 1:
+    print("Ingrese nombre de la persona a quien saludar: \n")
+    nom_persona=input()
+    apel_persona=input("Ingrese apellido de la persona a quien saludar: \n")
+    print("Hola, cómo estás ", nom_persona, apel_persona, "?")
+    print("\nDesea saludar a otra persona? : \n")
+    otro_saludo=int(input("ingrese 1 en caso afirmativo - 0 para salir: --> \n"))
